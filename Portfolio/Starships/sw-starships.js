cae8d13f33cddaf447b80cc6c64411c8298608ac
HjFluffy <91278027+HjFluffy@users.noreply.github.com>
2021-12-15 02:33:01 -0700

    Add files via upload

---
+++ Portfolio/Starships/sw-starships.js
@@ -0,0 +1,30 @@
+import { starships } from "../Data/starships.js";
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.displaySection')
+
+function populateView(starships) {
+    starships.forEach(starship => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
+        anchorWrap.addEventListener('click', () => {
+            populateShipView(starship)
+        })
+
+        
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+    })
+}
+
+populateView(starships)
+
+function populateShipView(shipData) {
+    let shipImage = document.createElement('img')
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/15.jpg`
+    shipView.appendChild(shipImage)
+}
